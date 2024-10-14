a048c6f4d756d97bf214c57bf06c41ed4909e729
Airlucky <luckytiwari07feb@gmail.com>
2024-10-14 14:50:09 +0530

    adding file

---
+++ Day1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Day1{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		char abc = sc.next().charAt(0);
+		float f1 = sc.nextFloat();
+		double d1 = sc.nextDouble();
+		
+		System.out.print(n);
+		System.out.print(abc);
+		System.out.print(f1);
+		System.out.print(d1);
+		
+	}
+}
